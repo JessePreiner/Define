09668f05814463edb5121ef2df70665b78d06b19
Jesse Preiner <jesse.preiner@yardi.com>
2018-01-26 13:38:39 -0600

    Initial

---
+++ define/Models.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+
+namespace define
+{
+    public class Metadata
+    {
+        public string provider { get; set; }
+    }
+
+    public class Example
+    {
+        public string text { get; set; }
+    }
+
+    public class Subsens
+    {
+        public List<string> definitions { get; set; }
+        public List<Example> examples { get; set; }
+        public string id { get; set; }
+        public List<string> regions { get; set; }
+    }
+
+    public class Sens
+    {
+        public List<string> definitions { get; set; }
+        public List<Example> examples { get; set; }
+        public string id { get; set; }
+        public List<Subsens> subsenses { get; set; }
+    }
+
+    public class VariantForm
+    {
+        public string text { get; set; }
+    }
+
+    public class GrammaticalFeature
+    {
+        public string text { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Entry
+    {
+        public List<string> etymologies { get; set; }
+        public string homographNumber { get; set; }
+        public List<Sens> senses { get; set; }
+        public List<VariantForm> variantForms { get; set; }
+        public List<GrammaticalFeature> grammaticalFeatures { get; set; }
+    }
+
+    public class Pronunciation
+    {
+        public List<string> dialects { get; set; }
+        public string phoneticNotation { get; set; }
+        public string phoneticSpelling { get; set; }
+        public string audioFile { get; set; }
+    }
+
+    public class LexicalEntry
+    {
+        public List<Entry> entries { get; set; }
+        public string language { get; set; }
+        public string lexicalCategory { get; set; }
+        public List<Pronunciation> pronunciations { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Result
+    {
+        public string id { get; set; }
+        public string language { get; set; }
+        public List<LexicalEntry> lexicalEntries { get; set; }
+        public string type { get; set; }
+        public string word { get; set; }
+    }
+
+    public class DictionaryApiResult
+    {
+        public Metadata metadata { get; set; }
+        public List<Result> results { get; set; }
+    }
+}
---
+++ define/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace define
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                throw new ApplicationException("Specify the URI of the resource to retrieve.");
+            }
+
+            var results = new Hashtable();
+            var uri = ConfigurationManager.AppSettings["api_url"];
+
+            foreach (var word in args)
+            {
+                var endpoint = string.Format(uri, word);
+                var client = new WebClient();
+                client.Headers.Add("app_id", ConfigurationManager.AppSettings["app_id"]);
+                client.Headers.Add("app_key", ConfigurationManager.AppSettings["app_key"]);
+                client.Headers.Add("Content-Type", "application/json");
+                try
+                {
+                    using (Stream data = client.OpenRead(endpoint))
+                    {
+                        using (StreamReader reader = new StreamReader(data))
+                        {
+                            string s = reader.ReadToEnd();
+                            using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(s)))
+                            {
+                                DataContractJsonSerializer des =
+                                    new DataContractJsonSerializer(typeof(DictionaryApiResult));
+                                DictionaryApiResult apiResult = (DictionaryApiResult) des.ReadObject(ms);
+
+                                results[word] = new ArrayList(apiResult.results
+                                    .SelectMany(result => result.lexicalEntries
+                                            .SelectMany(lexicalEntry => lexicalEntry.entries
+                                                    .SelectMany(entry => entry.senses
+                                                            .SelectMany(sense => sense?.definitions ?? new List<string>())
+                                                    )
+                                            )
+                                    ).ToList());
+                            }
+                        }
+                    }
+                }
+                catch (WebException webException)
+                {
+                    var response = (HttpWebResponse) webException.Response;
+                    switch (response.StatusCode)
+                    {
+                        case HttpStatusCode.NotFound:
+                            results[word] = new ArrayList { "No entry found"};
+                            break;
+                        case HttpStatusCode.InternalServerError:
+                            results[word] = "Unable to contact server";
+                            break;
+                    }
+
+                }
+
+                catch (Exception generalException)
+                {
+                    Console.WriteLine("Api call error");
+                    Console.WriteLine(generalException.GetBaseException().ToString());
+                }
+
+            }
+
+            var i = 1;
+            var sortedKeys = new ArrayList(results.Keys);
+            sortedKeys.Sort();
+            foreach (var key in sortedKeys)
+            {
+                Console.WriteLine(key);
+
+                foreach (var definition in (ArrayList)results[key])
+                {
+                    Console.WriteLine($"\t{i}.\t{definition}");
+                    i++;
+                        
+                }
+            }
+        }
+
+
+    }
+}
